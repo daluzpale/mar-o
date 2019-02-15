268965bd5eebd0519a283544f39275608a08e1be
daluzpale <46968675+daluzpale@users.noreply.github.com>
2019-02-15 21:17:28 +0200

    Oriented Object Program - Python OOP

---
+++ 2_week.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self):
+         self.nome = "Alexandre"
+         self.apelido = "Pale"
+         self.idade = 22
+         self.sexo = "Masculino"
+         self.signo ="Virgem"
+
+P = Pessoa()
+print("Nome: ",P.idade)
+print("Apelido: ",P.apelido)
+print("Idade: ", P.idade)
+print("Sexo: ",P.sexo)
+print("Signo: ", P.signo)
+print("\n" *5)
+print("Cumprimentos!")
